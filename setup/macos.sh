b115431c580d9dc5e1cece4d90808866dd7690f8
Daniel Kessl <dan@kessl.net>
2021-10-26 15:22:08 +0200

    Set a reasonably fast keyboard repeat rate

--- setup/macos.sh
+++ setup/macos.sh
@@ -130,9 +130,9 @@ defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 # Disable press-and-hold for keys in favor of key repeat
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
-# Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+# Set a reasonably fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 10
+defaults write NSGlobalDomain InitialKeyRepeat -int 100
 
 # Set language and text formats
 # Note: if you’re in the US, replace `EUR` with `USD`, `Centimeters` with
