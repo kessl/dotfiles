e746d124d8d676e765c7abddecffaa0c49e457b1
Daniel Kessl <dan@kessl.net>
2022-10-20 11:59:35 +0200

    Open link from iterm & to localhost in Firefox Developer edition

--- .finicky.js
+++ .finicky.js
@@ -2,8 +2,19 @@ module.exports = {
   defaultBrowser: "Firefox",
   handlers: [
     {
+      // open links from Slack in Firefox Developer Edition
       match: ({ opener }) => opener.bundleId === "com.tinyspeck.slackmacgap",
       browser: "Firefox Developer Edition",
+    },
+    {
+      // open links from iTerm in Firefox Developer Edition
+      match: ({ opener }) => opener.bundleId === "com.googlecode.iterm2",
+      browser: "Firefox Developer Edition",
+    },
+    {
+      // open localhost links in Firefox Developer Edition
+      match: ["http://127.0.0.1*"],
+      browser: "Firefox Developer Edition"
     },
   ],
 };
